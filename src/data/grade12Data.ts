7f810da4a2241af569b14b280d224c25fb6ac893
Ha Nguyen <nttha1910@gmail.com>
2026-04-03 21:31:46 +0700

    Update grade12Data.ts

--- src/data/grade12Data.ts
+++ src/data/grade12Data.ts
@@ -231,7 +231,7 @@ export const grade12Data = [
     chapter: "Chủ đề 2: Carbohydrate",
     cid: 7865,
     image2D:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Alpha-D-Glucopyranose.svg/640px-Alpha-D-Glucopyranose.svg.png",
+      "Alpha-D-Glucopyranose.svg (1)",
   },
   {
     id: "β-glucose",
